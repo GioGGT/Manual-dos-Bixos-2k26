a6ed2a7ce551b1c2fea4482c21f7aa9e9a67a02b
Vinicius32C <vinicupola@gmail.com>
2026-01-14 20:42:13 -0300

    Adjust leaf generation parameters and timing oloko acertou msm a ia

--- paginas/curso_ma.js
+++ paginas/curso_ma.js
@@ -6,13 +6,14 @@ function leaf(){
     l.src = type == 0? "../imgs/ma/leaf1.png" : "../imgs/ma/leaf2.png";
     l.style.setProperty("--r", -20 + "deg");
     l.style.left = Math.floor(Math.random()*100) + "%"
-    l.style.height = Math.floor(Math.random()*20) + 20 + "px"
+    l.style.height = Math.floor(Math.random()*120) + 120 + "px"
     l.style.setProperty("--h", Math.floor(Math.random() * (30 - 5 + 1)) + 105 + "vh")
     lc.appendChild(l);
-    setTimeout(leaf, 400)
+    setTimeout(leaf, 600)
     l.addEventListener("animationend", () => {
         l.remove();
     })
 }
+
 
 document.addEventListener("DOMContentLoaded", () =>{leaf()})
