58e34c4420727ab799884884d8196b07b84383ca
Kobrastar <juliomandrade43@gmail.com>
2026-01-14 22:54:59 -0300

    Pagina de MA melhorada
    Praticamente definitivo

--- paginas/curso_ma.js
+++ paginas/curso_ma.js
@@ -1,22 +1,32 @@
 const lc = document.getElementById('leafCont');
-function leaf(){
+
+function leaf() {
     const l = document.createElement("img");
-    const type = Math.floor(Math.random() * 2)
+    const type = Math.floor(Math.random() * 2);
+
     l.className = "leaf";
-    l.src = type == 0? "../imgs/ma/leaf1.png" : "../imgs/ma/leaf2.png";
-    l.style.setProperty("--r", -20 + "deg");
-    l.style.left = Math.floor(Math.random()*100) + "%"
-    l.style.height = Math.floor(Math.random()*60) + 60 + "px"
-    l.style.setProperty("--h", Math.floor(Math.random() * (30 - 5 + 1)) + 125 + "vh")
+    l.src = type === 0
+        ? "../imgs/ma/leaf1.png"
+        : "../imgs/ma/leaf2.png";
+
+    l.style.left = Math.random() * 100 + "%";
+    l.style.setProperty("--x", (Math.random() * 60 - 30) + "px");
+    l.style.setProperty("--r", (Math.random() * 360) + "deg");
+    l.style.setProperty("--h", (100 + Math.random() * 40) + "vh");
+
+    l.style.setProperty("--t", (18 + Math.random() * 12) + "s");
+    l.style.setProperty("--s", (4 + Math.random() * 3) + "s");
+
+    l.style.setProperty("--sc", (0.35 + Math.random() * 0.25));
+    l.style.setProperty("--o", (0.2 + Math.random() * 0.25));
+    l.style.setProperty("--b", Math.random() * 0.6 + "px");
+
     lc.appendChild(l);
-    setTimeout(leaf, 2000)
-    l.addEventListener("animationend", () => {
-        l.remove();
-    })
+
+    l.addEventListener("animationend", () => l.remove());
+
+    setTimeout(leaf, 800);
+
 }
 
-
-document.addEventListener("DOMContentLoaded", () =>{leaf()})
-
-
-
+document.addEventListener("DOMContentLoaded", leaf);
